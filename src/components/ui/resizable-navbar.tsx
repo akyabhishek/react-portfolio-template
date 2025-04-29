5a130f90ca69acf388282eb62a7f1a0faa10a31f
Abhishek Kumar Yadav <37768757+akyabhishek@users.noreply.github.com>
2025-04-29 20:26:04 +0530

    cosmetic fixes

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -154,8 +154,8 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
           ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
           : "none",
         width: visible ? "100%" : "100%",
-        paddingRight: visible ? "10px" : "0px",
-        paddingLeft: visible ? "10px" : "0px",
+        paddingRight: visible ? "15px" : "10px",
+        paddingLeft: visible ? "15px" : "10px",
         y: visible ? 0 : 0,
       }}
       transition={{
