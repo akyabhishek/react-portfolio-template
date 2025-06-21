17cbdd1a7104b6ecd9196277af7391ead5f1f5f8
Abhishek Kumar Yadav <37768757+akyabhishek@users.noreply.github.com>
2025-06-21 21:20:16 +0530

    vercel analytics config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import {RouterProvider} from "react-router-dom";
 import {ThemeProvider} from "./components/theme-provider";
 import router from "./config/routes";
-
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
+      <Analytics />
     </ThemeProvider>
   );
 }
