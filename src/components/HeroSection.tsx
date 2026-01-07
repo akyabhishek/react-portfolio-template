96bc7feb5bfa77791a55727aae57695a79ae6b86
Abhishek Kumar Yadav <37768757+akyabhishek@users.noreply.github.com>
2026-01-07 10:16:06 +0530

    linkedin followers updated and portfolio thumb updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -57,7 +57,7 @@ export default function HeroSection(): JSX.Element {
       animateValue(0, 2, 1500, (val) =>
         setAnimatedStats((prev) => ({ ...prev, experience: val }))
       );
-      animateValue(0, 6000, 2000, (val) =>
+      animateValue(0, 7000, 2000, (val) =>
         setAnimatedStats((prev) => ({ ...prev, followers: val }))
       );
       animateValue(0, 30, 1200, (val) =>
